184185632583a9dd8e8f4de65013c3e8c336a9eb
marco.spasiano <marco.spasiano@cnr.it>
2024-07-05 18:53:57 +0200

    Filtro di Stato in funzione del Nome della Regola- Closed Issue  #8

--- src/app/core/search/search.component.ts
+++ src/app/core/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SelectControlOption } from 'design-angular-kit';
@@ -37,13 +37,14 @@ export class SearchComponent implements OnInit {
               private ruleService: RuleService,
               private resultService: ResultService,
               private translateService: TranslateService,
+              private changeDetectorRef: ChangeDetectorRef,
               private datepipe: DatePipe,
               protected router: Router) {
     this.translateService.get('it').subscribe((labels: any) => {
       this.options.push({ value: 'company.codiceIpa', text: labels.company.codiceIpa });
       this.options.push({ value: 'company.denominazioneEnte', text: labels.company.denominazioneEnte });
       this.options.push({ value: 'createdAt,desc', text: labels.order.createdAt.desc });
-      this.optionsStatus.push({value: '', text: '*', selected: false});
+      this.optionsStatus.push({value: '', text: '*', disabled: false});
       this.optionsRule.push({value: '', text: '*', selected: false});
       this.optionsWorkflow.push({value: '', text: '*', selected: false});
       this.optionsCategoria.push({ value: '', text: '*', selected: true});
@@ -51,7 +52,23 @@ export class SearchComponent implements OnInit {
     Object.keys(CodiceCategoria).forEach((key) => {
       this.optionsCategoria.push({ value: key, text: CodiceCategoria[key]});
     });
+  }
 
+  isStatusDisabled(status: number, ruleName: string): boolean {
+    if (ruleName === Rule.AMMINISTRAZIONE_TRASPARENTE) {
+      return status == 404;
+    } 
+    return (status != 200 && status != 404);
+  }
+
+  manageOptionStatus(ruleName: string) {
+    this.optionsStatus.forEach((option: any) => {
+      option.disabled = this.isStatusDisabled(option.value, ruleName);
+      if (this.filterFormSearch.value.status === option.value && option.disabled) {
+        this.filterFormSearch.controls.status.patchValue('');
+      }
+    });    
+    this.changeDetectorRef.detectChanges();
   }
 
   ngOnInit(): void {
@@ -68,17 +85,20 @@ export class SearchComponent implements OnInit {
         codiceCategoria: new FormControl(),
         sort: new FormControl(queryParams.sort),
       });
-      Object.values(Status).filter(key => !isNaN(Number(key))).forEach((key) => {
+      this.filterFormSearch.valueChanges.subscribe((value: any) => {
+        this.manageOptionStatus(value.ruleName);
+      });  
+
+      Object.values(Status).filter(key => !isNaN(Number(key))).forEach((key: number) => {
         let status = String(key);
-        let isSelected = status === queryParams['status'];
         this.translateService.get(`it.rule.status.${status}`).subscribe((label: any) => {
           this.optionsStatus.push({
             value: status,
             text: label.ruletitle,
-            selected: isSelected
+            disabled: this.isStatusDisabled(key, ruleName)
           });
         });
-      });
+      });      
       this.ruleService.getRules().subscribe((rule) => {
         let rules: SelectRule[] = rule.getKeys(undefined, Rule.AMMINISTRAZIONE_TRASPARENTE, [], -1);
         Object.keys(rules).forEach((index) => {
