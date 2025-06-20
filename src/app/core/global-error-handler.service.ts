4233dad788579cf353b3b8278d68614a353a523b
mspasiano <marco.spasiano@gmail.com>
2025-06-20 17:54:57 +0200

    fix: 401 when token expired

--- src/app/core/global-error-handler.service.ts
+++ src/app/core/global-error-handler.service.ts
@@ -1,16 +1,23 @@
 import { ErrorHandler, Injectable} from '@angular/core';
 import { Router } from '@angular/router';
 import { SpringError } from '../common/model/spring-error.model';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+  ) { }
 
   handleError(error: any) {
     if ((error instanceof SpringError && error?.redirectOnError) || !(error instanceof SpringError)) {
       if (error?.httpErrorResponse?.status === 401) {
-        this.router.navigate(['error/unauthorized']);
+        if (environment.oidc.enable) {
+          this.router.navigate(['auth/signin']);
+        } else {
+          this.router.navigate(['error/unauthorized']);
+        }
       } else {
         this.router.navigate(['error/server-error']);
       }
